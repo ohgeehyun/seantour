ff25d8e72bc72022b7f6e2e65563233d73b5e7d6
ohgeehyun <chlghksn94@naver.com>
2020-07-07 15:21:21 +0900

    혼잡도 관련 수정 2차
    혼잡도 관련 수정 2차

--- src/main/java/geocni/travel/route/service/TravelMainServiceImpl.java
+++ src/main/java/geocni/travel/route/service/TravelMainServiceImpl.java
@@ -68,7 +68,6 @@ public class TravelMainServiceImpl extends EgovAbstractServiceImpl implements Tr
 	@Override
 	public List<?> selectBeachPerCntapi() throws Exception {
 		//DB 호출해서 정보값 가져오도록 할것.
-		//DB 호출해서 정보값 가져오도록 할것.
 		Calendar cal = Calendar.getInstance();
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmm");
 		String datestr = sdf.format(cal.getTime());
@@ -94,6 +93,7 @@ public class TravelMainServiceImpl extends EgovAbstractServiceImpl implements Tr
 			{
 			datestrtimechange= "0"+ String.valueOf(Integer.valueOf(datestrtimechange)-1);
 			}
+			datestrtimechange= String.valueOf((Integer.parseInt(datestrtimechange)-1));
 			datestr = datestrtemp + datestrtimechange+"30";
 		}else {
 			datestr = Integer.valueOf(datestr.substring(0, 10)) + "00";
